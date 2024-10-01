f760d44120f16275017eb3ced1123b682982ad39
shafikovrr <shafikov.r.r@ya.ru>
2024-10-01 10:18:45 +0500

    add commit

--- python_bot/lesson1/tasks/task1.py
+++ python_bot/lesson1/tasks/task1.py
@@ -1,3 +1,8 @@
 data = input('Введите дату: ')
 task = input('Введите задачу: ')
 print(data, task)
+
+# или
+
+a = data + ' ' + task
+print(a)
--- python_bot/lesson1/tasks/task3.py
+++ python_bot/lesson1/tasks/task3.py
@@ -1,11 +1,11 @@
-dictionary = {}
-data1 = input('Введите дату: ')
-task1 = input('Введите задачу: ')
+dictionary = {} # создаем пустой словарь
+data1 = input('Введите дату: ') # вводим значение переменной data1
+task1 = input('Введите задачу: ') # вводим значение переменной task1
 data2 = input('Введите дату: ')
 task2 = input('Введите задачу: ')
 data3 = input('Введите дату: ')
 task3 = input('Введите задачу: ')
-dictionary[data1] = task1
+dictionary[data1] = task1 # вводим в словарь ключь и значение (data1 = task1)
 dictionary[data2] = task2
 dictionary[data3] = task3
-print(dictionary)
+print(dictionary) # ввыводим на экран заполненный словарь
