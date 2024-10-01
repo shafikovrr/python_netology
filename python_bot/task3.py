914abf64a49ca80341ff2107441a234d47f06127
shafikovrr <shafikov.r.r@ya.ru>
2024-10-01 09:22:24 +0500

    add task(2-3)

--- python_bot/task3.py
+++ python_bot/task3.py
@@ -1,9 +1,11 @@
+dictionary = {}
 data1 = input('Введите дату: ')
 task1 = input('Введите задачу: ')
 data2 = input('Введите дату: ')
 task2 = input('Введите задачу: ')
 data3 = input('Введите дату: ')
 task3 = input('Введите задачу: ')
-print(data1, task1)
-print(data2, task2)
-print(data3, task3)
+dictionary[data1] = task1
+dictionary[data2] = task2
+dictionary[data3] = task3
+print(dictionary)
