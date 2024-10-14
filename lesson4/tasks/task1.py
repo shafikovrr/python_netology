553fdb56cc2fa9ee04f11e5f60c0207b340551f7
shafikovrr <shafikov.r.r@ya.ru>
2024-10-14 11:09:48 +0500

    add task1

---
+++ lesson4/tasks/task1.py
@@ -0,0 +1,22 @@
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+
+geo_logs_russia = {}
+geo_logs_russia.setdefault('visit', [])
+for visit in geo_logs:
+    for russia in visit:
+        if 'Россия' in visit[russia]:
+            geo_logs_russia['visit'].append(visit[russia])
+print(geo_logs_russia)
+   
+
